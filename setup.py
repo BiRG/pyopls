75f6b1ea71d7164eae9d91a3260eab10e636ef1c
Daniel Foose <dpfoose@gmail.com>
2019-07-16 16:48:21 -0400

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pyopls',
-    version='19.06',
+    version='19.07',
     author='BiRG @ Wright State University',
     author_email='foose.3@wright.edu',
     description='Orthogonal Projection to Latent Structures',
