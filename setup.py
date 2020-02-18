84fbbfba4eb930d2e4c6f956d9e3149834ee1a1d
Daniel Foose <dpfoose@gmail.com>
2020-02-18 17:47:45 -0500

    removed python 3.5 support because I want to use f-strings

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
     classifiers=[
         'Natural Language :: English',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
